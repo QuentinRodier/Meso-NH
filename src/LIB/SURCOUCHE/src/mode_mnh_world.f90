b999dfde42f42f482e0f2284671809d49404b3c2
Juan Escobar <juan.escobar@aero.obs-mip.fr>
2014-01-27 15:57:31 +0000

    Juan 27/01/2014: add header MNH_LIC to all SURCOUCHE files

--- src/LIB/SURCOUCHE/src/mode_mnh_world.f90
+++ src/LIB/SURCOUCHE/src/mode_mnh_world.f90
@@ -1,3 +1,7 @@
+!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
+!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!MNH_LIC for details. version 1.
 MODULE MODE_MNH_WORLD
   IMPLICIT NONE
   CHARACTER(len=*), parameter   :: conf_mnh_world="conf_mnh_world.nam"
