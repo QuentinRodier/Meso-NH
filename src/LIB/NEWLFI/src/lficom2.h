807953d61b1cd701c628106692fc5b1fc1ac41b3
Juan Escobar <juan.escobar@aero.obs-mip.fr>
2014-01-28 14:25:06 +0000

    Juan 27/01/2014: add header MNH_LIC to all LFI files

--- src/LIB/NEWLFI/src/lficom2.h
+++ src/LIB/NEWLFI/src/lficom2.h
@@ -1,3 +1,7 @@
+!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
+!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!MNH_LIC for details. version 1.
 C
 C       FONCTION SERVANT A RENDRE FATALE OU NON UNE ERREUR DETECTEE,
 C       A L'AIDE DU CODE-REPONSE COURANT, DU NIVEAU DE FILTRAGE GLOBAL,
