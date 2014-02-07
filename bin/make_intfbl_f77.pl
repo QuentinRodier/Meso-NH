756abe7da1405721ed2c767d9ec43fd41a995f13
Juan Escobar <juan.escobar@aero.obs-mip.fr>
2014-02-07 16:03:19 +0000

    Juan 7/02/2014: add header SURFEX_LIC to all auto modi perl files

--- bin/make_intfbl_f77.pl
+++ bin/make_intfbl_f77.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl
+#SURFEX_LIC Copyright 1994-2014 Meteo-France 
+#SURFEX_LIC This is part of the SURFEX software governed by the CeCILL-C  licence
+#SURFEX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
+#SURFEX_LIC for details. version 1.
 
 use strict;
 use warnings;
