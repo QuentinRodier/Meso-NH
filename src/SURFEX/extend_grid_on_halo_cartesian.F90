3071c35e559e58714ec36e30473c37b36a7db314
Gaelle Tanguy <gaelle.tanguy@meteo.fr>
2015-12-02 13:59:58 +0000

    Gaelle 2/12/2015 : licence

--- src/SURFEX/extend_grid_on_halo_cartesian.F90
+++ src/SURFEX/extend_grid_on_halo_cartesian.F90
@@ -1,4 +1,7 @@
-
+!SURFEX_LIC Copyright 1994-2014 Meteo-France 
+!SURFEX_LIC This is part of the SURFEX software governed by the CeCILL-C  licence
+!SURFEX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
+!SURFEX_LIC for details. version 1.
 !     ###########################################################
       SUBROUTINE EXTEND_GRID_ON_HALO_CARTESIAN(HPROGRAM,KDIM_FULL,KSIZE_FULL,KGRID_PAR,PGRID_PAR)
 !     ###########################################################
