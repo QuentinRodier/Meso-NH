7806c78c10de505faeaf6f7ce4e7edb2c4d1116f
Gaelle Tanguy <gaelle.tanguy@meteo.fr>
2016-01-19 12:57:22 +0000

    Gaelle 19/01/2016 : oubi licence

--- src/SURFEX/writesurf_snapn.F90
+++ src/SURFEX/writesurf_snapn.F90
@@ -1,4 +1,10 @@
-
+!SURFEX_LIC Copyright 1994-2014 Meteo-France 
+!SURFEX_LIC This is part of the SURFEX software governed by the CeCILL-C  licence
+!SURFEX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
+!SURFEX_LIC for details. version 1.
+!
+!
+!
 !!    MODIFICATIONS
 !!      M.Moge    01/2016  using WRITE_SURF_FIELD2D/3D for 2D/3D surfex fields writes
 !     #########
