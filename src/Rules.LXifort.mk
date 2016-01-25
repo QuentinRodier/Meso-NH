dc27fa7defc6e8b69ba85e0c0dd83c0afc13fb62
Juan Escobar <juan.escobar@aero.obs-mip.fr>
2016-01-25 14:31:27 +0000

    Juan 25/01/2016: better management of DEBUG compilation ( avoid O1 flag )

--- src/Rules.LXifort.mk
+++ src/Rules.LXifort.mk
@@ -146,9 +146,11 @@ include Makefile.MESONH.mk
 #                                                        #
 ##########################################################
 # Juan & Maud 20/03/2008 --> Ifort 10.1.008 Bug O2 optimization
-OPT_PERF1  =  -O1
+ifneq "$(OPTLEVEL)" "DEBUG"
+OPT_PERF1  =  -O1 
 OBJS_O1= spll_schu.o spll_ps2str.o spll_p_abs.o spll_ini_one_way_n.o spll_urban_solar_abs.o
 $(OBJS_O1) : OPT = $(OPT_BASE) $(OPT_PERF1)
+endif
 
 ifneq "$(findstring 8,$(LFI_INT))" ""
 OBJS_I8=spll_NEWLFI_ALL.o
