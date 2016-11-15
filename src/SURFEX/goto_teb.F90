6668d2636db1dbdef6674f7dda985fac6772daca
Gaelle TANGUY <gaelle.tanguy@meteo.fr>
2016-11-15 11:58:02 +0100

    G.Delautier 15/11/2016 : correct bug compilation

--- src/SURFEX/goto_teb.F90
+++ src/SURFEX/goto_teb.F90
@@ -2,6 +2,6 @@
 !SFX_LIC This is part of the SURFEX software governed by the CeCILL-C licence
 !SFX_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !SFX_LIC for details. version 1.
-SUBROUTINE GOTO_TEB!
+SUBROUTINE GOTO_TEB
 !!
 END SUBROUTINE GOTO_TEB
