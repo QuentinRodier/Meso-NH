e34af44a5010cfdc59b2926398f7c8a8384c274f
Philippe WAUTELET <philippe.wautelet@aero.obs-mip.fr>
2018-02-13 11:18:28 +0100

    Philippe 13/02/2018: comment OpenMP include/use if compiled without OpenMP

--- src/SURFEX/modd_surfex_omp.F90
+++ src/SURFEX/modd_surfex_omp.F90
@@ -36,13 +36,13 @@ USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK
 USE PARKIND1  ,ONLY : JPRB
 !
 #ifdef AIX64 
- USE OMP_LIB
+!$ USE OMP_LIB
 #endif
 !
 IMPLICIT NONE
 !
 #ifndef AIX64
-  INCLUDE 'omp_lib.h'
+!$  INCLUDE 'omp_lib.h'
 #endif
 !
 !RJ: this broke non openmp version before
