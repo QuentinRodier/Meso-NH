5b206a53b7f731a7c4164ac8f8d959e54f45b5a6
Philippe WAUTELET <philippe.wautelet@aero.obs-mip.fr>
2018-04-17 15:42:45 +0200

    Philippe 17/04/2018: IO: corrections for modi_fm, modi_io and MODI_MENU_DIACHRO

--- src/LIB/SURCOUCHE/src/modi_io.f90
+++ src/LIB/SURCOUCHE/src/modi_io.f90
@@ -1,15 +1,10 @@
-!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 1994-2018 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !MNH_LIC for details. version 1.
 !-----------------------------------------------------------------
-!--------------- special set of characters for CVS information
-!-----------------------------------------------------------------
-! $Source$
-! $Name$ 
-! $Revision$ 
-! $Date$
-!-----------------------------------------------------------------
+! Modifications:
+!  Philippe Wautelet: 05/2016-04/2018: new data structures and calls for I/O
 !-----------------------------------------------------------------
 
 MODULE MODI_IO_ll
@@ -33,7 +28,8 @@ INTERFACE
        PAD,      &
        KNB_PROCIO,&
        KMELEV,&
-       OPARALLELIO)
+       OPARALLELIO, &
+       HPROGRAM_ORIG)
 
     USE MODD_IO_ll, ONLY : TFILEDATA
 
@@ -52,16 +48,18 @@ INTERFACE
     INTEGER,         INTENT(IN),  OPTIONAL :: KNB_PROCIO
     INTEGER(KIND=LFI_INT), INTENT(IN),  OPTIONAL :: KMELEV
     LOGICAL,         INTENT(IN),  OPTIONAL :: OPARALLELIO
+    CHARACTER(LEN=*),INTENT(IN),  OPTIONAL :: HPROGRAM_ORIG !To emulate a file coming from this program
   END SUBROUTINE OPEN_ll
   
-  SUBROUTINE CLOSE_ll(TPFILE,IOSTAT,STATUS,OPARALLELIO)
+  SUBROUTINE CLOSE_ll(TPFILE,IOSTAT,STATUS,OPARALLELIO,HPROGRAM_ORIG)
   USE MODD_IO_ll, ONLY : TFILEDATA
 
-  TYPE(TFILEDATA),  INTENT(INOUT)         :: TPFILE
-  INTEGER,          INTENT(OUT), OPTIONAL :: IOSTAT
-  CHARACTER(LEN=*), INTENT(IN),  OPTIONAL :: STATUS
-  LOGICAL,          INTENT(IN),  OPTIONAL :: OPARALLELIO
-  END SUBROUTINE CLOSE_ll
+    TYPE(TFILEDATA),  INTENT(IN)            :: TPFILE
+    INTEGER,          INTENT(OUT), OPTIONAL :: IOSTAT
+    CHARACTER(LEN=*), INTENT(IN),  OPTIONAL :: STATUS
+    LOGICAL,          INTENT(IN),  OPTIONAL :: OPARALLELIO
+    CHARACTER(LEN=*), INTENT(IN),  OPTIONAL :: HPROGRAM_ORIG !To emulate a file coming from this program
+ END SUBROUTINE CLOSE_ll
 
 END INTERFACE
 !
