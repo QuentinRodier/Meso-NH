1e22d1d463c11534bd15f31b996c2a9a19b97e9c
Philippe WAUTELET <philippe.wautelet@aero.obs-mip.fr>
2018-05-29 15:06:34 +0200

    Philippe 29/05/2018: IO: force EMIS field read/write to 2D

--- src/MNH/ini_surf_rad.f90
+++ src/MNH/ini_surf_rad.f90
@@ -1,4 +1,4 @@
-!MNH_LIC Copyright 1994-2018 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 2003-2018 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !MNH_LIC for details. version 1.
@@ -65,7 +65,9 @@ END MODULE MODI_INI_SURF_RAD
 !
 USE MODD_IO_ll, ONLY : TFILEDATA
 !
+USE MODE_FIELD, ONLY: FIND_FIELD_ID_FROM_MNHNAME, TFIELDDATA, TFIELDLIST
 USE MODE_FMREAD
+USE MODE_MSG
 !
 IMPLICIT NONE
 !
@@ -74,16 +76,25 @@ IMPLICIT NONE
 TYPE(TFILEDATA),        INTENT(IN)  :: TPINIFILE ! Initial file
 REAL, DIMENSION(:,:,:), INTENT(OUT) :: PDIR_ALB  ! Direct albedo
 REAL, DIMENSION(:,:,:), INTENT(OUT) :: PSCA_ALB  ! Diffuse albedo
-REAL, DIMENSION(:,:,:),   INTENT(OUT) :: PEMIS     ! emissivity
+REAL, DIMENSION(:,:,:), INTENT(OUT) :: PEMIS     ! emissivity
 REAL, DIMENSION(:,:),   INTENT(OUT) :: PTSRAD    ! radiative surface temperature
 !
 !*       0.2   declarations of local variables
 !
+INTEGER           :: IID, IRESP
+TYPE(TFIELDDATA)  :: TZFIELD
 !-------------------------------------------------------------------------------
 !
 CALL IO_READ_FIELD(TPINIFILE,'DIR_ALB',PDIR_ALB)
 CALL IO_READ_FIELD(TPINIFILE,'SCA_ALB',PSCA_ALB)
-CALL IO_READ_FIELD(TPINIFILE,'EMIS',PEMIS)
+!
+CALL PRINT_MSG(NVERB_INFO,'IO','INI_SURF_RAD','EMIS: reading only first band (copy on others)')
+CALL FIND_FIELD_ID_FROM_MNHNAME('EMIS',IID,IRESP)
+TZFIELD = TFIELDLIST(IID)
+TZFIELD%NDIMS = 2
+CALL IO_READ_FIELD(TPINIFILE,TZFIELD,PEMIS(:,:,1))
+PEMIS(:,:,:) = SPREAD(SOURCE=PEMIS(:,:,1),DIM=3,NCOPIES=SIZE(PEMIS,3))
+!
 CALL IO_READ_FIELD(TPINIFILE,'TSRAD',PTSRAD)
 !  
 !-------------------------------------------------------------------------------
