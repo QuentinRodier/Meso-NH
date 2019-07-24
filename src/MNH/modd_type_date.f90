3c8c839976d389a0bf0e5b5a642c0e7af2e1f23b
Philippe WAUTELET <philippe.wautelet@aero.obs-mip.fr>
2019-07-24 14:22:34 +0200

    Philippe 24/07/2019: set default values for DATE_TIME type

--- src/MNH/modd_type_date.f90
+++ src/MNH/modd_type_date.f90
@@ -1,12 +1,7 @@
-!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 1997-2019 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
-!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
 !MNH_LIC for details. version 1.
-!-----------------------------------------------------------------
-!--------------- special set of characters for RCS information
-!-----------------------------------------------------------------
-! $Source$ $Revision$
-! MASDEV4_7 modd 2006/05/18 13:07:25
 !-----------------------------------------------------------------
 !     #################
       MODULE MODD_TYPE_DATE
@@ -17,7 +12,7 @@
 !!    PURPOSE
 !!    -------
 !       The purpose of this declarative module is to define
-!      the time types. 
+!      the time types
 !
 !!
 !!**  IMPLICIT ARGUMENTS
@@ -27,31 +22,33 @@
 !!    REFERENCE
 !!    --------- 
 !!      Book2 of documentation of Meso-NH (module MODD_TYPE_DATE)
-!!       
+!!
 !!    AUTHOR
 !!    ------
 !!	P. Jabouille   *Meteo France*
 !!
 !!    MODIFICATIONS
 !!    -------------
-!!      Original    11/08/97                      
+!!      Original    11/08/97
+!  P. Wautelet 24/07/2019: set default values
 !-------------------------------------------------------------------------------
 !
 !*       0.   DECLARATIONS
 !             ------------
 !
-!
+use modd_parameters, only: NNEGUNDEF, XNEGUNDEF
+
 IMPLICIT NONE
 !
 TYPE DATE
-INTEGER :: YEAR
-INTEGER :: MONTH
-INTEGER :: DAY
+  INTEGER :: YEAR  = NNEGUNDEF
+  INTEGER :: MONTH = 0
+  INTEGER :: DAY   = 0
 END TYPE DATE
 !
 TYPE DATE_TIME
-TYPE (DATE) :: TDATE
-REAL :: TIME
-END TYPE DATE_TIME 
+  TYPE (DATE) :: TDATE
+  REAL        :: TIME = XNEGUNDEF
+END TYPE DATE_TIME
 !
 END MODULE MODD_TYPE_DATE
