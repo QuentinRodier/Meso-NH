QuentinRodier/Meso-NH

commit 1dc08bf3a1f7514e4ba636851b86cb83f06cf86d

Philippe 13/09/2019: budget: simplify and modernize date/time management

src/MNH/write_seriesn.f90
@@ -60,7 +60,8 @@ END MODULE MODI_WRITE_SERIES_n
 !!      J.Escobar : 15/09/2015 : WENO5 & JPHEXT <> 1 
 !!      P.Wautelet: 11/07/2016 : removed MNH_NCWRIT define
 !!  Philippe Wautelet: 05/2016-04/2018: new data structures and calls for I/O
-!!
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
+!
 !-------------------------------------------------------------------------------
 !
 !
@@ -235,11 +236,11 @@ ENDIF
 !*      2.3  Write in diachro file
 !
 GICP=.TRUE. ; GJCP=.TRUE. ; GKCP=.TRUE.
-CALL WRITE_DIACHRO(TPDIAFILE,TLUOUT,'TSERIES','CART',NSGRIDD1,XSDATIME(:,1:NSNBSTEPT),   &
-                   XSSERIES1(1:1,1:1,1:1,1:NSNBSTEPT,:,:),               &
-                   XSTRAJT(1:NSNBSTEPT,:),CSTITLE1,CSUNIT1,CSCOMMENT1,   &
-                   GICP,GJCP,GKCP,                               &
-                   KIL=1,KIH=1,KJL=1,KJH=1,KKL=1,KKH=1)
+CALL WRITE_DIACHRO( TPDIAFILE, TLUOUT, 'TSERIES', 'CART', NSGRIDD1, tpsdates(1:nsnbstept), &
+                    XSSERIES1(1:1,1:1,1:1,1:NSNBSTEPT,:,:),                                &
+                    CSTITLE1(:), CSUNIT1(:), CSCOMMENT1(:),                                &
+                    OICP = GICP, OJCP = GJCP, OKCP = GKCP,                                 &
+                    KIL = 1, KIH = 1, KJL = 1, KJH = 1, KKL = 1, KKH = 1                   )
 !
 !----------------------------------------------------------------------------
 !
@@ -289,11 +290,11 @@ DEALLOCATE(ZVAR3D)
 !*      3.2  Write in diachro file
 !
 GICP=.TRUE. ; GJCP=.TRUE. ; GKCP=.FALSE.
-CALL WRITE_DIACHRO(TPDIAFILE,TLUOUT,'ZTSERIES','CART',NSGRIDD2,XSDATIME(:,1:NSNBSTEPT),   &
-                   XSSERIES2(1:1,1:1,1:IKMAX,1:NSNBSTEPT,:,:),            &
-                   XSTRAJT(1:NSNBSTEPT,:),CSTITLE2,CSUNIT2,CSCOMMENT2,    &
-                   GICP,GJCP,GKCP,                 &
-                   KIL=1,KIH=1,KJL=1,KJH=1,KKL=IKB,KKH=IKE)
+CALL WRITE_DIACHRO( TPDIAFILE, TLUOUT, 'ZTSERIES', 'CART', NSGRIDD2, tpsdates(1:nsnbstept), &
+                    XSSERIES2(1:1,1:1,1:1,1:NSNBSTEPT,:,:),                                 &
+                    CSTITLE2(:), CSUNIT2(:), CSCOMMENT2(:),                                 &
+                    OICP = GICP, OJCP = GJCP, OKCP = GKCP,                                  &
+                    KIL = 1, KIH = 1, KJL = 1, KJH = 1, KKL = IKB, KKH = IKE                )
 !
 !----------------------------------------------------------------------------
 !
@@ -347,11 +348,11 @@ DO JS=1,NBJSLICE
     YSTITLE3S(JT)=ADJUSTL(ADJUSTR(CSTITLE3(JT))//'Y'//YSL//'-'//YSH)
   END DO
   GICP=.FALSE. ; GJCP=.TRUE. ; GKCP=.TRUE.
-  CALL WRITE_DIACHRO(TPDIAFILE,TLUOUT,YGROUP,'CART',NSGRIDD3,XSDATIME(:,1:NSNBSTEPT),    &
-                      ZSERIES3_ll(1:IIU_ll,1:1,1:1,1:NSNBSTEPT,1:1,ISB1:ISB2),&
-                      XSTRAJT(1:NSNBSTEPT,:),YSTITLE3S,CSUNIT3,CSCOMMENT3,     &
-                      GICP,GJCP,GKCP,                               &
-                      KIL=1,KIH=IIU_ll,KJL=1,KJH=1,KKL=1,KKH=1)
+  CALL WRITE_DIACHRO( TPDIAFILE, TLUOUT, YGROUP, 'CART', NSGRIDD3, tpsdates(1:nsnbstept), &
+                      ZSERIES3_ll(1:IIU_ll,1:1,1:1,1:NSNBSTEPT,1:1,ISB1:ISB2),            &
+                      YSTITLE3S(:), CSUNIT3(:), CSCOMMENT3(:),                            &
+                      OICP = GICP, OJCP = GJCP, OKCP = GKCP,                              &
+                      KIL = 1, KIH = IIU_ll, KJL = 1, KJH = 1, KKL = 1, KKH = 1           )
 END DO
 DEALLOCATE(ZVAR3D,ZWORK2D,ZSERIES3_ll)
 !

src/MNH/aircraft_balloon_evol.f90
@@ -10,20 +10,14 @@ MODULE MODI_AIRCRAFT_BALLOON_EVOL
 INTERFACE
 !
       SUBROUTINE AIRCRAFT_BALLOON_EVOL(PTSTEP,               &
-                       TPDTEXP, TPDTMOD, TPDTSEG, TPDTCUR,   &
                        PXHAT, PYHAT, PZ,                     &
                        PMAP, PLONOR, PLATOR,                 &
                        PU, PV, PW, PP, PTH, PR, PSV, PTKE,   &
                        PTS, PRHODREF, PCIT,TPFLYER, PSEA     )
 !
-USE MODD_TYPE_DATE
 USE MODD_AIRCRAFT_BALLOON
 !
 REAL,                     INTENT(IN)     :: PTSTEP ! time step
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTEXP! experiment date and time
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTMOD! model start date and time
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTSEG! segment date and time
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTCUR! current date and time
 REAL, DIMENSION(:),       INTENT(IN)     :: PXHAT  ! x coordinate
 REAL, DIMENSION(:),       INTENT(IN)     :: PYHAT  ! y coordinate
 REAL, DIMENSION(:,:,:),   INTENT(IN)     :: PZ     ! z array
@@ -55,7 +49,6 @@ END MODULE MODI_AIRCRAFT_BALLOON_EVOL
 !
 !     ########################################################
       SUBROUTINE AIRCRAFT_BALLOON_EVOL(PTSTEP,               &
-                       TPDTEXP, TPDTMOD, TPDTSEG, TPDTCUR,   &
                        PXHAT, PYHAT, PZ,                     &
                        PMAP, PLONOR, PLATOR,                 &
                        PU, PV, PW, PP, PTH, PR, PSV, PTKE,   &
@@ -128,7 +121,8 @@ END MODULE MODI_AIRCRAFT_BALLOON_EVOL
 !!      October, 2016 (G.DELAUTIER) LIMA
 !!     March,28, 2018 (P. Wautelet) replace TEMPORAL_DIST by DATETIME_DISTANCE
 !!  Philippe Wautelet: 05/2016-04/2018: new data structures and calls for I/O
-!!
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
+!
 !! --------------------------------------------------------------------------
 !       
 !*      0. DECLARATIONS
@@ -167,9 +161,9 @@ USE MODD_RAIN_ICE_DESCR,   ONLY: XALPHAR_I=>XALPHAR,XNUR_I=>XNUR,XLBEXR_I=>XLBEX
                                  XLBI_I=>XLBI,XAI_I=>XAI,XBI_I=>XBI,XC_I_I=>XC_I,&
                                  XRTMIN_I=>XRTMIN,XCONC_LAND,XCONC_SEA
 USE MODD_REF_n,            ONLY: XRHODREF
-USE MODD_TIME
+USE MODD_TIME,             only: tdtexp
+USE MODD_TIME_n,           only: tdtcur
 USE MODD_TURB_FLUX_AIRCRAFT_BALLOON
-USE MODD_TYPE_DATE
 !
 USE MODE_DATETIME
 USE MODE_FGAU,             ONLY: GAULAG
@@ -188,10 +182,6 @@ IMPLICIT NONE
 !
 !
 REAL,                     INTENT(IN)     :: PTSTEP ! time step
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTEXP! experiment date and time
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTMOD! model start date and time
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTSEG! segment date and time
-TYPE(DATE_TIME),          INTENT(IN)     :: TPDTCUR! current date and time
 REAL, DIMENSION(:),       INTENT(IN)     :: PXHAT  ! x coordinate
 REAL, DIMENSION(:),       INTENT(IN)     :: PYHAT  ! y coordinate
 REAL, DIMENSION(:,:,:),   INTENT(IN)     :: PZ     ! z array
@@ -220,7 +210,6 @@ REAL, DIMENSION(:,:),     INTENT(IN)     :: PSEA
 !
 INTEGER :: IMI        ! model index
 REAL    :: ZTHIS_PROC ! 1 if balloon is currently treated by this proc., else 0
-REAL    :: ZTIMEEXP   ! elpased time between start of experiment and segment
 !
 INTEGER :: IIB        ! current processor domain sizes
 INTEGER :: IJB
@@ -398,7 +387,7 @@ ZYHATM(  IJU  )=1.5*PYHAT(  IJU  )-0.5*PYHAT(  IJU-1)
 !*      2.3  Compute time until launch by comparison of dates and times
 !            ----------------------------------------------------------
 !
-CALL DATETIME_DISTANCE(TPFLYER%LAUNCH,TPDTCUR,ZTDIST)
+CALL DATETIME_DISTANCE(TPFLYER%LAUNCH,TDTCUR,ZTDIST)
 !
 !*      3.   LAUNCH
 !            ------
@@ -412,7 +401,7 @@ IF (.NOT. TPFLYER%FLY) THEN
 !*      3.1  comparison of dates and times
 !            -----------------------------
 !
-!  CALL DATETIME_DISTANCE(TPFLYER%LAUNCH,TPDTCUR,ZTDIST)
+!  CALL DATETIME_DISTANCE(TPFLYER%LAUNCH,TDTCUR,ZTDIST)
 !
 !*      3.2  launch/takeoff is effective
 !            ---------------------------
@@ -446,8 +435,8 @@ IF (.NOT. TPFLYER%FLY) THEN
         IF (ZTDIST <= PTSTEP ) THEN
           WRITE(ILUOUT,*) '-------------------------------------------------------------------'
           WRITE(ILUOUT,*) 'Aircraft ',TPFLYER%TITLE,' takes off the   ',      &
-                      TPDTCUR%TDATE%DAY,'/',TPDTCUR%TDATE%MONTH,'/',      &
-                      TPDTCUR%TDATE%YEAR,' at ',NINT(TPDTCUR%TIME),' sec.'
+                      TDTCUR%TDATE%DAY,'/',TDTCUR%TDATE%MONTH,'/',      &
+                      TDTCUR%TDATE%YEAR,' at ',NINT(TDTCUR%TIME),' sec.'
           WRITE(ILUOUT,*) '-------------------------------------------------------------------'
         ENDIF
       ENDIF
@@ -456,8 +445,8 @@ IF (.NOT. TPFLYER%FLY) THEN
       GLAUNCH     = .TRUE.
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
       WRITE(ILUOUT,*) 'Balloon  ',TPFLYER%TITLE,' is launched the ',      &
-                    TPDTCUR%TDATE%DAY,'/',TPDTCUR%TDATE%MONTH,'/',      &
-                    TPDTCUR%TDATE%YEAR,' at ',NINT(TPDTCUR%TIME),' sec.'
+                    TDTCUR%TDATE%DAY,'/',TDTCUR%TDATE%MONTH,'/',      &
+                    TDTCUR%TDATE%YEAR,' at ',NINT(TDTCUR%TIME),' sec.'
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
     END IF
 !
@@ -502,25 +491,14 @@ END IF
 !
 IF (GSTORE) THEN
   IN = TPFLYER%N_CUR
-  CALL DATETIME_DISTANCE(TDTEXP,TDTSEG,ZTIMEEXP)
-  !
-  TPFLYER%TIME(IN) = (IN-1) * TPFLYER%STEP + ZTIMEEXP
-  TPFLYER%DATIME( 1,IN) = TPDTEXP%TDATE%YEAR
-  TPFLYER%DATIME( 2,IN) = TPDTEXP%TDATE%MONTH
-  TPFLYER%DATIME( 3,IN) = TPDTEXP%TDATE%DAY
-  TPFLYER%DATIME( 4,IN) = TPDTEXP%TIME
-  TPFLYER%DATIME( 5,IN) = TPDTSEG%TDATE%YEAR
-  TPFLYER%DATIME( 6,IN) = TPDTSEG%TDATE%MONTH
-  TPFLYER%DATIME( 7,IN) = TPDTSEG%TDATE%DAY
-  TPFLYER%DATIME( 8,IN) = TPDTSEG%TIME
-  TPFLYER%DATIME( 9,IN) = TPDTMOD%TDATE%YEAR
-  TPFLYER%DATIME(10,IN) = TPDTMOD%TDATE%MONTH
-  TPFLYER%DATIME(11,IN) = TPDTMOD%TDATE%DAY
-  TPFLYER%DATIME(12,IN) = TPDTMOD%TIME
-  TPFLYER%DATIME(13,IN) = TPDTCUR%TDATE%YEAR
-  TPFLYER%DATIME(14,IN) = TPDTCUR%TDATE%MONTH
-  TPFLYER%DATIME(15,IN) = TPDTCUR%TDATE%DAY
-  TPFLYER%DATIME(16,IN) = TPDTCUR%TIME
+#if 0
+  tpflyer%tpdates(in)%date%year  = tdtexp%date%year
+  tpflyer%tpdates(in)%date%month = tdtexp%date%month
+  tpflyer%tpdates(in)%date%day   = tdtexp%date%day
+  tpflyer%tpdates(in)%time       = tdtexp%time + ( in - 1 ) * tpflyer%step
+#else
+  tpflyer%tpdates(in) = tdtcur
+#endif
 END IF
 !
 IF ( TPFLYER%FLY) THEN
@@ -829,19 +807,19 @@ IF ( TPFLYER%FLY) THEN
     TPFLYER%FLY = .FALSE.
     IF (TPFLYER%TYPE=='AIRCRA' .AND. .NOT. GLAUNCH ) THEN
       WRITE(ILUOUT,*) 'Aircraft ',TPFLYER%TITLE,' flew out of the domain the ', &
-                    TPDTCUR%TDATE%DAY,'/',TPDTCUR%TDATE%MONTH,'/',            &
-                    TPDTCUR%TDATE%YEAR,' at ',TPDTCUR%TIME,' sec.'
+                    TDTCUR%TDATE%DAY,'/',TDTCUR%TDATE%MONTH,'/',            &
+                    TDTCUR%TDATE%YEAR,' at ',TDTCUR%TIME,' sec.'
     ELSE IF (TPFLYER%TYPE /= 'AIRCRA') THEN
       WRITE(ILUOUT,*) 'Balloon ',TPFLYER%TITLE,' crashed the ',                 &
-                    TPDTCUR%TDATE%DAY,'/',TPDTCUR%TDATE%MONTH,'/',            &
-                    TPDTCUR%TDATE%YEAR,' at ',TPDTCUR%TIME,' sec.'
+                    TDTCUR%TDATE%DAY,'/',TDTCUR%TDATE%MONTH,'/',            &
+                    TDTCUR%TDATE%YEAR,' at ',TDTCUR%TIME,' sec.'
     END IF
   ELSE
     IF (TPFLYER%TYPE=='AIRCRA' .AND. .NOT. GLAUNCH .AND. ZTDIST > PTSTEP ) THEN
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
       WRITE(ILUOUT,*) 'Aircraft ',TPFLYER%TITLE,' flies  in leg',TPFLYER%SEGCURN ,' the ',  &
-        TPDTCUR%TDATE%DAY,'/',TPDTCUR%TDATE%MONTH,'/',      &
-        TPDTCUR%TDATE%YEAR,' at ',NINT(TPDTCUR%TIME),' sec.'
+        TDTCUR%TDATE%DAY,'/',TDTCUR%TDATE%MONTH,'/',      &
+        TDTCUR%TDATE%YEAR,' at ',NINT(TDTCUR%TIME),' sec.'
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
     ENDIF
 !
@@ -1708,12 +1686,12 @@ IF (TPFLYER%NMODEL /= IMODEL) THEN
    IF (NDAD(IMODEL) == TPFLYER%NMODEL) THEN
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
       WRITE(ILUOUT,*) TPFLYER%TITLE,' comes from model ',IMODEL,' in  model ',&
-             TPFLYER%NMODEL,' at ',NINT(TPDTCUR%TIME),' sec.'
+             TPFLYER%NMODEL,' at ',NINT(TDTCUR%TIME),' sec.'
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
    ELSE
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
       WRITE(ILUOUT,*) TPFLYER%TITLE,' goes from model ',IMODEL,' to  model ',&
-             TPFLYER%NMODEL,' at ',NINT(TPDTCUR%TIME),' sec.'
+             TPFLYER%NMODEL,' at ',NINT(TDTCUR%TIME),' sec.'
       WRITE(ILUOUT,*) '-------------------------------------------------------------------'
    ENDIF
 ENDIF

src/MNH/diag.f90
@@ -92,6 +92,7 @@
 !  P. Wautelet 11/02/2019: added missing use of MODI_CH_MONITOR_n
 !  P. Wautelet 28/03/2019: use MNHTIME for time measurement variables
 !  P. Wautelet 26/07/2019: bug correction: deallocate of zsea and ztown done too early
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
 !-------------------------------------------------------------------------------
 !
 !*       0.     DECLARATIONS
@@ -177,6 +178,7 @@ IMPLICIT NONE
 !*       0.1   declarations of local variables
 !
 TYPE(DATE_TIME)   :: TXDTBAL   ! current time and date for BALLOON and AIRCRAFT trajectories
+TYPE(DATE_TIME)   :: TPDTCUR_SAVE
 CHARACTER (LEN=28), DIMENSION(1) :: YINIFILE ! names of the INPUT FM-file
 CHARACTER (LEN=28), DIMENSION(1) :: YINIFILEPGD ! names of the INPUT FM-file
 CHARACTER (LEN=5)  :: YSUFFIX   ! character string for the OUTPUT FM-file number
@@ -533,11 +535,14 @@ IF ( LAIRCRAFT_BALLOON ) THEN
   WRITE(ILUOUT0,*) 'DIAG AFTER OPEN DIACHRONIC FILE'
   WRITE(ILUOUT0,*) ' '
 !
+  TPDTCUR_SAVE = TDTCUR
+!
   TXDTBAL%TDATE%YEAR  = TDTCUR%TDATE%YEAR
   TXDTBAL%TDATE%MONTH = TDTCUR%TDATE%MONTH
   TXDTBAL%TDATE%DAY   = TDTCUR%TDATE%DAY
   TXDTBAL%TIME        = TDTCUR%TIME - NTIME_AIRCRAFT_BALLOON/2.
   CALL DATETIME_CORRECTDATE(TXDTBAL)
+  TDTCUR = TXDTBAL !TDTCUR is used in AIRCRAFT_BALLOON
 !
   ALLOCATE (ZSEA(SIZE(XRHODJ,1),SIZE(XRHODJ,2)))
   ALLOCATE (ZTOWN(SIZE(XRHODJ,1),SIZE(XRHODJ,2)))
@@ -546,15 +551,18 @@ IF ( LAIRCRAFT_BALLOON ) THEN
   CALL MNHGET_SURF_PARAM_n (PSEA=ZSEA(:,:),PTOWN=ZTOWN(:,:))
   DO ISTEPBAL=1,NTIME_AIRCRAFT_BALLOON,INT(XSTEP_AIRCRAFT_BALLOON)
     CALL AIRCRAFT_BALLOON(XSTEP_AIRCRAFT_BALLOON,                &
-                      TDTEXP, TDTMOD, TDTCUR, TXDTBAL,           &
                       XXHAT, XYHAT, XZZ, XMAP, XLONORI, XLATORI, &
                       XUT, XVT, XWT, XPABST, XTHT, XRT, XSVT,    &
                       XTKET, XTSRAD, XRHODREF,XCIT,ZSEA)
-!
+
     TXDTBAL%TIME=TXDTBAL%TIME + XSTEP_AIRCRAFT_BALLOON
     CALL DATETIME_CORRECTDATE(TXDTBAL)
+    TDTCUR = TXDTBAL !TDTCUR is used in AIRCRAFT_BALLOON
   ENDDO
   DEALLOCATE (ZSEA,ZTOWN)
+!
+  TDTCUR = TPDTCUR_SAVE
+!
   CALL IO_Header_write(TZDIACFILE)
   CALL WRITE_LFIFMN_FORDIACHRO_n(TZDIACFILE)
   CALL WRITE_AIRCRAFT_BALLOON(TZDIACFILE)

src/MNH/ini_aircraft_balloon.f90
@@ -74,6 +74,7 @@ END MODULE MODI_INI_AIRCRAFT_BALLOON
 !!               March, 2013 : O.Caumont, C.Lac : add vertical profiles
 !!               OCT,2016 : G.Delautier LIMA
 !!  Philippe Wautelet: 05/2016-04/2018: new data structures and calls for I/O
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
 !!
 !! --------------------------------------------------------------------------
 !
@@ -355,7 +356,7 @@ IF (TPFLYER%NMODEL > 0) THEN
 ENDIF
 !
 !
-ALLOCATE(TPFLYER%TIME(ISTORE))
+allocate( tpflyer%tpdates(istore) )
 ALLOCATE(TPFLYER%X   (ISTORE))
 ALLOCATE(TPFLYER%Y   (ISTORE))
 ALLOCATE(TPFLYER%Z   (ISTORE))
@@ -390,13 +391,11 @@ END IF
 ALLOCATE(TPFLYER%TKE_DISS(ISTORE))
 ALLOCATE(TPFLYER%TSRAD (ISTORE))
 ALLOCATE(TPFLYER%ZS  (ISTORE))
-ALLOCATE(TPFLYER%DATIME(16,ISTORE))
 !
 ALLOCATE(TPFLYER%THW_FLUX  (ISTORE))
 ALLOCATE(TPFLYER%RCW_FLUX  (ISTORE))
 ALLOCATE(TPFLYER%SVW_FLUX  (ISTORE,KSV))
 !
-TPFLYER%TIME     = XUNDEF
 TPFLYER%X        = XUNDEF
 TPFLYER%Y        = XUNDEF
 TPFLYER%Z        = XUNDEF
@@ -428,23 +427,6 @@ TPFLYER%TKE      = XUNDEF
 TPFLYER%TSRAD    = XUNDEF
 TPFLYER%ZS       = XUNDEF
 TPFLYER%TKE_DISS = XUNDEF
-TPFLYER%DATIME( 1,1:ISTORE) = TPDTSEG%TDATE%YEAR
-TPFLYER%DATIME( 2,1:ISTORE) = TPDTSEG%TDATE%MONTH
-TPFLYER%DATIME( 3,1:ISTORE) = TPDTSEG%TDATE%DAY
-TPFLYER%DATIME( 4,1:ISTORE) = TPDTSEG%TIME
-TPFLYER%DATIME( 5,1:ISTORE) = TPDTSEG%TDATE%YEAR
-TPFLYER%DATIME( 6,1:ISTORE) = TPDTSEG%TDATE%MONTH
-TPFLYER%DATIME( 7,1:ISTORE) = TPDTSEG%TDATE%DAY
-TPFLYER%DATIME( 8,1:ISTORE) = TPDTSEG%TIME
-TPFLYER%DATIME( 9,1:ISTORE) = TPDTSEG%TDATE%YEAR
-TPFLYER%DATIME(10,1:ISTORE) = TPDTSEG%TDATE%MONTH
-TPFLYER%DATIME(11,1:ISTORE) = TPDTSEG%TDATE%DAY
-TPFLYER%DATIME(12,1:ISTORE) = TPDTSEG%TIME
-TPFLYER%DATIME(13,1:ISTORE) = TPDTSEG%TDATE%YEAR
-TPFLYER%DATIME(14,1:ISTORE) = TPDTSEG%TDATE%MONTH
-TPFLYER%DATIME(15,1:ISTORE) = TPDTSEG%TDATE%DAY
-TPFLYER%DATIME(16,1:ISTORE) = XUNDEF
-
 !
 TPFLYER%THW_FLUX        = XUNDEF
 TPFLYER%RCW_FLUX        = XUNDEF

src/MNH/ini_lesn.f90
@@ -35,6 +35,7 @@
 !!  Philippe Wautelet: 05/2016-04/2018: new data structures and calls for I/O
 !!                     02/2019 (C. Lac) Add rain fraction as a LES diagnostic
 !  P. Wautelet 10/04/2019: replace ABORT and STOP calls by Print_msg
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
 !! --------------------------------------------------------------------------
 !
 !*      0. DECLARATIONS
@@ -338,17 +339,13 @@ NLES_TIMES = ( INT( (XSEGLEN-XTSTEP+1.E-6) / XTSTEP ) ) / NLES_DTCOUNT
 !
 NLES_TCOUNT = 0
 !
-!*      3.6  date array for diachro
+!*      3.6  dates array for diachro
 !            ----------------------
 !
-ALLOCATE(XLES_DATIME(16,NLES_TIMES))
+allocate( xles_dates( nles_times ) )
+allocate( xles_times( nles_times ) )
 !
-!*      3.7  sampling times array for diachro
-!            --------------------------------
-!
-ALLOCATE(XLES_TRAJT(NLES_TIMES,1))
-!
-!*      3.8  No data
+!*      3.7  No data
 !            -------
 !
 IF (NLES_TIMES==0) THEN

src/MNH/ini_modeln.f90
@@ -287,6 +287,7 @@ END MODULE MODI_INI_MODEL_n
 !  P. Wautelet 10/04/2019: replace ABORT and STOP calls by Print_msg
 !  P. Wautelet 19/04/2019: removed unused dummy arguments and variables
 !  P. Wautelet 07/06/2019: allocate lookup tables for optical properties only when needed
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
 !---------------------------------------------------------------------------------
 !
 !*       0.    DECLARATIONS
@@ -2397,18 +2398,18 @@ CALL INI_AIRCRAFT_BALLOON(TPINIFILE,XTSTEP, TDTSEG, XSEGLEN, NRR, NSV, &
 !*      24.     STATION initializations
 !              -----------------------
 !
-CALL INI_SURFSTATION_n(XTSTEP, TDTSEG, XSEGLEN, NRR, NSV, &
-                       CTURB=="TKEL" ,                    &
-                       XLATORI, XLONORI                   )
+CALL INI_SURFSTATION_n(XTSTEP, XSEGLEN, NRR, NSV, &
+                       CTURB=="TKEL" ,            &
+                       XLATORI, XLONORI           )
 !
 !-------------------------------------------------------------------------------
 !
 !*      25.     PROFILER initializations
 !              ------------------------
 !
-CALL INI_POSPROFILER_n(XTSTEP, TDTSEG, XSEGLEN, NRR, NSV,  &
-                       CTURB=="TKEL",                      &
-                       XLATORI, XLONORI                    )
+CALL INI_POSPROFILER_n(XTSTEP, XSEGLEN, NRR, NSV,  &
+                       CTURB=="TKEL",              &
+                       XLATORI, XLONORI            )
 !
 !-------------------------------------------------------------------------------
 !

src/MNH/ini_posprofilern.f90
@@ -9,14 +9,11 @@ MODULE MODI_INI_POSPROFILER_n
 !
 INTERFACE
 !
-      SUBROUTINE INI_POSPROFILER_n(PTSTEP, TPDTSEG, PSEGLEN, &
+      SUBROUTINE INI_POSPROFILER_n(PTSTEP, PSEGLEN,          &
                                    KRR, KSV, OUSETKE,        &
                                    PLATOR, PLONOR            )
 !
-USE MODD_TYPE_DATE
-!
 REAL,               INTENT(IN) :: PTSTEP  ! time step
-TYPE(DATE_TIME),    INTENT(IN) :: TPDTSEG ! segment date and time
 REAL,               INTENT(IN) :: PSEGLEN ! segment length
 INTEGER,            INTENT(IN) :: KRR     ! number of moist variables
 INTEGER,            INTENT(IN) :: KSV     ! number of scalar variables
@@ -33,7 +30,7 @@ END INTERFACE
 END MODULE MODI_INI_POSPROFILER_n
 !
 !     ########################################################
-      SUBROUTINE INI_POSPROFILER_n(PTSTEP, TPDTSEG, PSEGLEN, &
+      SUBROUTINE INI_POSPROFILER_n(PTSTEP, PSEGLEN,          &
                                    KRR, KSV, OUSETKE,        &
                                    PLATOR, PLONOR            )
 !     ########################################################
@@ -67,6 +64,7 @@ END MODULE MODI_INI_POSPROFILER_n
 !!     P. Tulet 15/01/2002  
 !!     C.Lac 10/2016  Add visibility diagnostic
 !!  Philippe Wautelet: 05/2016-04/2018: new data structures and calls for I/O
+!  P. Wautelet 13/09/2019: budget: simplify and modernize date/time management
 !! --------------------------------------------------------------------------
 !       
 !*      0. DECLARATIONS
@@ -81,7 +79,6 @@ USE MODD_PARAMETERS
 USE MODD_PROFILER_n
 USE MODD_RADIATIONS_n, ONLY: NAER
 USE MODD_TYPE_PROFILER
-USE MODD_TYPE_DATE
 !
 USE MODE_GRIDPROJ
 USE MODE_ll
@@ -96,7 +93,6 @@ IMPLICIT NONE
 !
 !
 REAL,               INTENT(IN) :: PTSTEP  ! time step
-TYPE(DATE_TIME),    INTENT(IN) :: TPDTSEG ! segment date and time
 REAL,               INTENT(IN) :: PSEGLEN ! segment length
 INTEGER,            INTENT(IN) :: KRR     ! number of moist variables
 INTEGER,            INTENT(IN) :: KSV     ! number of scalar variables
@@ -161,7 +157,7 @@ TYPE(PROFILER), INTENT(INOUT) :: TPROFILER
 !
 ISTORE = INT ( (PSEGLEN-XTSTEP) / TPROFILER%STEP ) + 1
 !
-ALLOCATE(TPROFILER%TIME  (ISTORE))
+allocate( tprofiler%tpdates( istore ) )
 ALLOCATE(TPROFILER%ERROR (NUMBPROFILER))
 ALLOCATE(TPROFILER%X     (NUMBPROFILER))
 ALLOCATE(TPROFILER%Y     (NUMBPROFILER))
@@ -186,7 +182,6 @@ IF (OUSETKE) THEN
 ELSE
   ALLOCATE(TPROFILER%TKE (0,IKU,0))
 END IF
-ALLOCATE(TPROFILER%DATIME(16,ISTORE))
 ALLOCATE(TPROFILER%T2M     (ISTORE,NUMBPROFILER))
 ALLOCATE(TPROFILER%Q2M     (ISTORE,NUMBPROFILER))
 ALLOCATE(TPROFILER%HU2M    (ISTORE,NUMBPROFILER))
@@ -209,7 +204,6 @@ ALLOCATE(TPROFILER%TKE_DISS(ISTORE,IKU,NUMBPROFILER))
 !
 !
 TPROFILER%ERROR= .FALSE.
-TPROFILER%TIME = XUNDEF
 TPROFILER%ZON  = XUNDEF
 TPROFILER%MER  = XUNDEF
 TPROFILER%FF   = XUNDEF

commit 1c32155c5d0718a358c9d0314961d41e3b686d5b

Philippe 06/10/2020: IO: bugfix: correct XSSERIES2 range (bug introduced in commit 1dc08bf3a)

src/MNH/write_seriesn.f90
@@ -291,7 +291,7 @@ DEALLOCATE(ZVAR3D)
 !
 GICP=.TRUE. ; GJCP=.TRUE. ; GKCP=.FALSE.
 CALL WRITE_DIACHRO( TPDIAFILE, 'ZTSERIES', 'CART', NSGRIDD2, tpsdates(1:nsnbstept), &
-                    XSSERIES2(1:1,1:1,1:1,1:NSNBSTEPT,:,:),                         &
+                    XSSERIES2(1:1,1:1,1:IKMAX,1:NSNBSTEPT,:,:),                         &
                     CSTITLE2(:), CSUNIT2(:), CSCOMMENT2(:),                         &
                     OICP = GICP, OJCP = GJCP, OKCP = GKCP,                          &
                     KIL = 1, KIH = 1, KJL = 1, KJH = 1, KKL = IKB, KKH = IKE        )