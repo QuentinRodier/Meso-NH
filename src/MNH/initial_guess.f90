4b18b960e3c328446d5676d3da61e13eb67607ac
Philippe WAUTELET <philippe.wautelet@aero.obs-mip.fr>
2021-02-11 15:45:03 +0100

    Philippe 11/02/2021: budgets: bugfix: lbu_beg was modified at wrong place

--- src/MNH/initial_guess.f90
+++ src/MNH/initial_guess.f90
@@ -1,4 +1,4 @@
-!MNH_LIC Copyright 1994-2020 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 1994-2021 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
 !MNH_LIC for details. version 1.
@@ -277,9 +277,9 @@ IF (LBU_ENABLE) THEN
       end do
     end if
   end if
-END IF
 
-LBU_BEG=.FALSE.
+  LBU_BEG=.FALSE.
+END IF
 
 !-------------------------------------------------------------------------------
 !
