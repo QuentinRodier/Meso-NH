bfc48e360aa2597861659d3d5ac150363d6ca392
Quentin Rodier <quentin.rodier@meteo.fr>
2023-02-21 11:20:16 +0100

    Quentin 21/02/2023: minpack now included in PHYEX

--- src/Makefile.MESONH.mk
+++ src/Makefile.MESONH.mk
@@ -128,14 +128,6 @@ CPPFLAGS     += $(CPPFLAGS_SURCOUCHE)
 #ARCH_XYZ    := $(ARCH_XYZ)-$(VER_SURCOUCHE)
 endif
 ##########################################################
-#           Source MINPACK                             #
-##########################################################
-DIR_MINPACK += LIB/minpack
-#
-ifdef DIR_MINPACK
-DIR_MASTER   += $(DIR_MINPACK)
-endif
-##########################################################
 #           Source RAD                                   #
 ##########################################################
 # PRE_BUG TEST !!!
