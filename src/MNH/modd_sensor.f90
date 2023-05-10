4c85669ff32ac4c003027dfebd808893260cc0ee
Philippe WAUTELET <philippe.wautelet@aero.obs-mip.fr>
2023-05-10 13:34:24 +0200

    Philippe 10/05/2023: sensors: merge FLYER_INTERPZ and STATPROF_INTERP_3D into Interp_from_masspoint_vert

--- src/MNH/modd_sensor.f90
+++ src/MNH/modd_sensor.f90
@@ -39,6 +39,52 @@ MODULE MODD_SENSOR
       REAL :: XYMCOEF = XUNDEF ! Interpolation coefficient for Y (mass-point)
       REAL :: XXUCOEF = XUNDEF ! Interpolation coefficient for X (U-point)
       REAL :: XYVCOEF = XUNDEF ! Interpolation coefficient for Y (V-point)
+
+    CONTAINS
+      PROCEDURE                               :: INTERP_FROM_MASSPOINT_VERT
   END TYPE TSENSOR
+
+  CONTAINS
+    FUNCTION INTERP_FROM_MASSPOINT_VERT( TPSENSOR, PA ) RESULT( PB )
+      USE MODD_CONF,       ONLY: L1D
+
+      USE MODE_MSG
+
+      CLASS(TSENSOR),         INTENT(INOUT) :: TPSENSOR
+      REAL, DIMENSION(:,:,:), INTENT(IN)    :: PA
+      REAL, DIMENSION( SIZE(PA,3) )         :: PB
+
+      INTEGER :: JI, JJ, JK
+
+      IF ( SIZE( PA, 1 ) == 2 ) THEN
+        JI = 1
+        JJ = 1
+      ELSE IF ( L1D ) THEN
+        JI = 2
+        JJ = 2
+      ELSE
+        JI = TPSENSOR%NI_M
+        JJ = TPSENSOR%NJ_M
+      END IF
+
+      IF (       JI >= 1 .AND. JI < SIZE( PA, 1 ) &
+           .AND. JJ >= 1 .AND. JJ < SIZE( PA, 2 ) ) THEN
+        DO JK = 1, SIZE( PA, 3 )
+          IF ( PA(JI, JJ,   JK) /= XUNDEF .AND. PA(JI+1, JJ,   JK) /= XUNDEF .AND. &
+               PA(JI, JJ+1, JK) /= XUNDEF .AND. PA(JI+1, JJ+1, JK) /= XUNDEF       ) THEN
+            PB(JK) = (1.-TPSENSOR%XXMCOEF) *  (1.-TPSENSOR%XYMCOEF) * PA(JI,   JJ,   JK) + &
+                     (   TPSENSOR%XXMCOEF) *  (1.-TPSENSOR%XYMCOEF) * PA(JI+1, JJ,   JK) + &
+                     (1.-TPSENSOR%XXMCOEF) *  (   TPSENSOR%XYMCOEF) * PA(JI,   JJ+1, JK) + &
+                     (   TPSENSOR%XXMCOEF) *  (   TPSENSOR%XYMCOEF) * PA(JI+1, JJ+1, JK)
+          ELSE
+            PB(JK) = XUNDEF
+          END IF
+        END DO
+      ELSE
+        CALL PRINT_MSG( NVERB_ERROR, 'GEN', 'Interp_from_masspoint_vert', 'value can not be interpolated', OLOCAL = .TRUE. )
+        PB(:) = XUNDEF
+      END IF
+
+    END FUNCTION INTERP_FROM_MASSPOINT_VERT
 
 END MODULE MODD_SENSOR
