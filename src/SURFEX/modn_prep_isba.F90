e496ef7f972d9482661971c8854afa8b7c995c0b
Quentin Rodier <quentin.rodier@meteo.fr>
2023-09-12 11:44:46 +0200

    Revert "Robert Schoetter : writing per vegtype" This contribution was not finalized (Gaetan Noual's thesis), it will be commited by Patrick Lemoigne once ready through SURFEX offline repo This reverts commit a73037f2452639297bc6700f1eba07c18eb69bd4.

--- src/SURFEX/modn_prep_isba.F90
+++ src/SURFEX/modn_prep_isba.F90
@@ -35,20 +35,10 @@
 USE MODD_PREP_ISBA,  ONLY : CFILE_ISBA, CTYPE, CFILEPGD_ISBA, CTYPEPGD,       &
                               CFILE_HUG, CTYPE_HUG,                           &
                               CFILE_HUG_SURF, CFILE_HUG_ROOT, CFILE_HUG_DEEP, &
-                              CFILE_HUG_1, CFILE_HUG_2, CFILE_HUG_3,          &
-                              CFILE_HUG_4, CFILE_HUG_5, CFILE_HUG_6,          &
-                              CFILE_HUG_7, CFILE_HUG_8, CFILE_HUG_9,          &
-                              CFILE_HUG_10, CFILE_HUG_11, CFILE_HUG_12,       &
-                              CFILE_HUG_13, CFILE_HUG_14,                     &
                               XHUG_SURF, XHUG_ROOT, XHUG_DEEP,                 &
                               XHUGI_SURF, XHUGI_ROOT, XHUGI_DEEP,              &
                               CFILE_TG, CTYPE_TG,                             &
                               CFILE_TG_SURF, CFILE_TG_ROOT, CFILE_TG_DEEP,    &
-                              CFILE_TG_1, CFILE_TG_2, CFILE_TG_3,             &
-                              CFILE_TG_4, CFILE_TG_5, CFILE_TG_6,             &
-                              CFILE_TG_7, CFILE_TG_8, CFILE_TG_9,             &
-                              CFILE_TG_10, CFILE_TG_11, CFILE_TG_12,          &
-                              CFILE_TG_13, CFILE_TG_14,                       &
                               XTG_SURF, XTG_ROOT, XTG_DEEP,LEXTRAP_TG,LEXTRAP_WG,&
                               LEXTRAP_WGI,LEXTRAP_SN 
 
@@ -63,20 +53,10 @@ LOGICAL           :: LISBA_CANOPY !flag to use air layers inside the canopy
 NAMELIST/NAM_PREP_ISBA/CFILE_ISBA, CTYPE, CFILEPGD_ISBA, CTYPEPGD,       &
                          CFILE_HUG, CTYPE_HUG,                           &
                          CFILE_HUG_SURF, CFILE_HUG_ROOT, CFILE_HUG_DEEP, &
-                         CFILE_HUG_1, CFILE_HUG_2, CFILE_HUG_3,          &
-                         CFILE_HUG_4, CFILE_HUG_5, CFILE_HUG_6,          &
-                         CFILE_HUG_7, CFILE_HUG_8, CFILE_HUG_9,          &
-                         CFILE_HUG_10, CFILE_HUG_11, CFILE_HUG_12,       &
-                         CFILE_HUG_13, CFILE_HUG_14,                     &
                          XHUG_SURF, XHUG_ROOT, XHUG_DEEP,                &
                          XHUGI_SURF, XHUGI_ROOT, XHUGI_DEEP,             &
                          CFILE_TG, CTYPE_TG,                              &
                          CFILE_TG_SURF, CFILE_TG_ROOT, CFILE_TG_DEEP,    &
-                         CFILE_TG_1, CFILE_TG_2, CFILE_TG_3,             &
-                         CFILE_TG_4, CFILE_TG_5, CFILE_TG_6,             &
-                         CFILE_TG_7, CFILE_TG_8, CFILE_TG_9,             &
-                         CFILE_TG_10, CFILE_TG_11, CFILE_TG_12,          &
-                         CFILE_TG_13, CFILE_TG_14,                       &
                          XTG_SURF, XTG_ROOT, XTG_DEEP,                   &
                          NYEAR, NMONTH, NDAY, XTIME, LISBA_CANOPY,LEXTRAP_TG,&
                          LEXTRAP_WG,LEXTRAP_WGI,LEXTRAP_SN  
