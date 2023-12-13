638a11d6511af5fe2e97b3222691a9e6e6f10211
Batti Filippi <batti.filippi@gmail.com>
2023-12-13 17:27:30 +0100

    rebased to last branch

--- src/LIB/FOREFIRE/modn_forefire.f90
+++ src/LIB/FOREFIRE/modn_forefire.f90
@@ -1,4 +1,4 @@
-!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 1994-2023 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !MNH_LIC for details. version 1.
@@ -11,7 +11,8 @@
 !!    AUTHOR
 !!    ------
 !	           : P. Tulet (LACy / CNRM)
-!!              X. Pialat (SPE)
+!               X. Pialat (SPE)
+!               J.B.Filippi (SPE)
 !	Creation   : 09.10.2010
 !-------------------------------------------------------------------------------
 !
